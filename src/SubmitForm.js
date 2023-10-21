05aea48a43b5fafa556234ca5cd462e8d55db433
Andrei <ec22818@qmul.ac.uk>
2023-10-21 11:31:20 +0100

    .

---
+++ src/SubmitForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styles from './SubmitForm.css';
+
+const SubmitForm = () => {
+  // Form validation schema using Yup
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    phone: Yup.string().required('Phone number is required'),
+  });
+
+  // Form submission logic
+  const onSubmit = (values, { resetForm }) => {
+    // Send form data to the server or mock server/API
+    // Display success message
+    // Clear the form fields
+  };
+
+  return (
+    <div>
+      <h1>Submit Information</h1>
+      <Formik
+        initialValues={{ name: '', email: '', phone: '' }}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field type="text" id="email" name="email" />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone</label>
+            <Field type="text" id="phone" name="phone" />
+            <ErrorMessage name="phone" component="div" className="error" />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default SubmitForm;
